9fb453b84a73c513d891b55f5d5df859a06fd087
Abdul HAYEE KAMBOH <159583911+abdulhayeekamboh@users.noreply.github.com>
2025-02-12 01:17:42 +0500

    Delete KIMJI directory

--- KIMJI/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Saare session variables remove karega
-session_destroy(); // Session destroy karega
-header("Location: login.html"); // User ko homepage ya login page par redirect karega
-exit();
-?>
